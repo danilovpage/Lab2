75b836004e8b4fb724f181d44a7fd5508e5b84bc
danilovpage <danilovpage@gmail.com>
2021-10-31 03:22:03 +0400

    Добавлено считывание данных, переменные для подсчета статистики и валидация данных с прогрессом обработки в форме прогрессбара

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import time
 import argparse
 import json
 
-
 parser = argparse.ArgumentParser()
 parser.add_argument("input", help='Get path file input')
 parser.add_argument("output", help='Get path file output')
@@ -21,7 +20,7 @@ class Validator:
     def __init__(self):
         pass
 
-    def check_email(email : str) -> bool:
+    def check_email(email: str) -> bool:
         """
         Выполняет проверку корректности адреса электронной почты
         Если в строке присутствуют пробелы, запятые, двойные точки,
@@ -42,7 +41,7 @@ class Validator:
             return True
         return False
 
-    def check_weight(weight : int) -> bool:
+    def check_weight(weight: int) -> bool:
         """
         Выполняет проверку корректности веса
         Если вес выходит за пределы разумного возвращается False
@@ -62,7 +61,7 @@ class Validator:
             return True
         return False
 
-    def check_snils(snils : str)-> bool:
+    def check_snils(snils: str) -> bool:
         """
         Выполняет проверку номера снилса на корректность
         Если номер снилса не содержит 11 цифр то возвращается False
@@ -81,7 +80,7 @@ class Validator:
             return False
         return True
 
-    def check_passport_number(passport_number : int) -> bool:
+    def check_passport_number(passport_number: int) -> bool:
         """
         Выполняет проверку номера паспорта на корректность
         Если номер паспорта состоит не из 6 цифр то возвращается False
@@ -119,7 +118,7 @@ class Validator:
             return False
         return True
 
-    def check_age(age : int) -> bool:
+    def check_age(age: int) -> bool:
         """
         Выполняет проверку возраста на корректность
         Если возраст превышает отметку в 100 то возвращается False
@@ -139,7 +138,7 @@ class Validator:
                 return True
         return False
 
-    def check_address(address : str) -> bool:
+    def check_address(address: str) -> bool:
         """
         Выполняет проверку корректности адреса
         Если адрес не является строкой или указан не в формате "улица пробел номер дома" то возвращено False
@@ -159,4 +158,56 @@ class Validator:
             if re.match(pattern, address):
                 return True
         return False
+
+
+# считывание данных из файла с заданием в виде списка словарей
+data = json.load(open(args.input, encoding='windows-1251'))
+
+# переменные для подсчета статистики обработанных данных
+true_data = list()
+email = 0
+weight = 0
+snils = 0
+passport_number = 0
+university = 0
+age = 0
+political_views = 0
+worldview = 0
+addsress = 0
+
+# Валидация данных с прогрессбаром
+with tqdm(total=len(data)) as progressbar:
+    for person in data:
+        temp = True
+        if not Validator.check_email(person['email']):
+            email += 1
+            temp = False
+        if not Validator.check_weight(person['weight']):
+            weight += 1
+            temp = False
+        if not Validator.check_snils(person['snils']):
+            snils += 1
+            temp = False
+        if not Validator.check_passport_number(person['passport_number']):
+            passport_number += 1
+            temp = False
+        if not Validator.check_string(person["university"]):
+            university += 1
+            temp = False
+        if not Validator.check_age(person['age']):
+            age += 1
+            temp = False
+        if not Validator.check_string(person['political_views']):
+            political_views += 1
+            temp = False
+        if not Validator.check_string(person['worldview']):
+            worldview += 1
+            temp = False
+        if not Validator.check_address(person['address']):
+            addsress += 1
+            temp = False
+        if temp:
+            true_data.append(person)
+        progressbar.update(1)
+
 
